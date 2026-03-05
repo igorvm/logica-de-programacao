6e30c04df32eadda518d28304a33e402566c0fd6
Igor Santos <igoreduvalerio@gmail.com>
2026-03-05 13:22:19 -0300

    exercicios2

--- logica-programacao/02-logica-javascript/exercicios/exercicio2.js
+++ logica-programacao/02-logica-javascript/exercicios/exercicio2.js
@@ -1,19 +1,69 @@
-alert('Boas vindas ao jogo do número secreto');
+//Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
 
-// Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log('Valor do chute:', chute);
-
-let numeroSecreto = 4;
-
-// Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
-
-if (chute == numeroSecreto) {
-    alert('Acertou');
-    console.log('Resultado: Você tentou ' + chute + ' e o correto era ' + numeroSecreto);
-
+alert('Olá Mundo')
+let dia = prompt('Qual é o dia da semana?');
+if (dia === 'Sábado' || dia === 'Domingo') {
+    alert('Bom fim de semana!');
 } else {
-    // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
-    alert('O número secreto era ' + numeroSecreto);
-    console.log('Número secreto:', numeroSecreto);
+    alert('Boa semana!')
 }
+
+---
+
+//Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+let numero = prompt('Digite um número positivo ou negativo');
+
+if (numero > 0) {
+    alert('Número positivo!');
+} else if (numero < 0) {
+    alert('Número negativo!');
+} else {
+    alert('O número é zero.');
+}
+
+---
+
+//Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+
+let mensagemDeBoasVindas = ('Seja bem vindo ao jogo!')
+alert (mensagemDeBoasVindas);
+
+let nomeDoJogador = prompt('Qual seu usuario?');
+alert ('Olá, ' +nomeDoJogador + '! Vamos dar inicio ao jogo!');
+
+let pontuação = 0
+
+pontuação = (prompt('Qual a sua pontuação?')); 
+     if (pontuação >= 100) {
+    alert ('Parabéns, você venceu!');
+       console.log('Você conseguiu adquirir a pontuação para vencer!');
+     } else  {
+    alert ('Infelizmente, você perdeu! Tente novamente!');
+    console.log ('Você não atingiu a pontuação minima.');
+}
+
+---
+
+//Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+alert ('Bem vindo ao banco!')
+
+let nomeDoCliente = prompt ('Informe seu nome:')
+alert ('Olá ' + nomeDoCliente + ', é um prazer ter você de volta!');
+
+let saldo = ('R$ 1.350,00')
+
+let verSaldo = prompt ('Deseja ver seu saldo? Responda com Sim ou Não.')
+if (verSaldo === 'Sim') {
+    alert ('Seu saldo é: ' +saldo)
+} else if (verSaldo === 'Não') {
+    alert ('Tudo bem, ' + nomeDoCliente + '. Estaremos te aguardando!')
+}
+
+---
+
+// Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+
+let nome = prompt('Olá, registre seu nome á baixo.')
+alert('Bem vindo, ' + nome + '!');
